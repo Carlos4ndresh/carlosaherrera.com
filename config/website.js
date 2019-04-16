528837b408e44d5d16dbacd1695c5c8e12bfc020
Carlos Andrés Herrera <carlos.herrera@outlook.com>
2019-04-15 20:42:00 -0500

    modificados

--- config/website.js
+++ config/website.js
@@ -3,20 +3,17 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Solutions Architect', // Navigation and Site Title
+  siteTitle: 'Carlos A. Herrera - Developer at night, Solutions Architect ther rest of the time', // Navigation and Site Title
   siteTitleAlt: 'carlos4ndresh', // Alternative Site title for SEO
   siteTitleShort: 'Carlosandresh', // short_name for manifest
   siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
   siteUrl: 'https://www.carlosaherrera.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  siteLogo: '/src/images/monphoto_tn.jpg', // Used for SEO and manifest
+  siteDescription: '',
   author: 'Carlos A. Herrera', // Author for schema.org JSONLD
 
-  // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@Carlos4ndresh', // Twitter Username
-  // ogSiteName: 'cara', // Facebook Site Name
-  // ogLanguage: 'en_US', // Facebook Language
   googleAnalyticsID: 'UA-138228571-1',
 
   // Manifest and Progress color
