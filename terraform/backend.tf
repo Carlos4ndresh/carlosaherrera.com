c830748f129fe701f10d500031b4c0b2e939ec5f
Carlos Andrés Herrera Pérez <29789062+Carlos4ndresh@users.noreply.github.com>
2022-01-03 17:12:00 -0400

    Moved website infrastructure resources terraform code to new repo only for resources creation: `cahp-terraform-website`

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "terraform-cahpsite-state-bucket"
-        key    = "web_tfstate"
-        region = "us-east-2"
-
-    }
-    required_version = "~>0.12"
-}
--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "website_bucket" {
-    value = aws_s3_bucket.website_bucket.bucket_domain_name
-} 
-
-# output "badge_url" {
-#     value = module.pipeline.badge_url
-# }
